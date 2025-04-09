eaf44b05117e0f1219092c51d635194a95b1e159
Venturatto nao <rafaelcolombomiranda@gmail.com>
2025-04-08 22:08:08 -0300

    inicio aula 3 com classe

--- semaforo/src/main/java/semaforo/Light/SimpleLigth.java
+++ semaforo/src/main/java/semaforo/Light/SimpleLigth.java
@@ -2,7 +2,26 @@ package semaforo.Light;
 
 public class SimpleLigth implements Light {
 
+	private OnOff state = OnOff.OFF;
 	
+	@Override
+	public void turnOn() {
+		this.state = OnOff.ON;
+	}
 	
+	@Override
+	public void turnOff() {
+		this.state = OnOff.OFF;
+	}
+	
+	@Override
+	public boolean isOn() {
+		return this.state == OnOff.ON;
+	}
+	
+	@Override
+	public boolean isOff() {
+		return this.state == OnOff.OFF;
+	}
 	
 }
